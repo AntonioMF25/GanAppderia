1b6d52c6862c5d411e8f90976c28587f8ec025eb
Antonio Francisco Mulero Fernández <AntoFM25@gmail.com>
2022-04-21 18:05:34 +0200

    Proyecto base

---
+++ .Temp~/RollbackSQL.sql
@@ -0,0 +1,16 @@
+-- HistoricoProductos [ent3]
+drop table "public"."productos";
+-- Ubicacion [ent2]
+drop table "public"."ubicacion";
+-- Lineas Factura [ent11]
+drop table "public"."lineas_factura";
+-- Factura [ent10]
+drop table "public"."factura";
+-- HistoricoPromociones [ent1]
+drop table "public"."historicopromociones";
+-- User [User]
+drop table "public"."user";
+-- Module [Module]
+drop table "public"."module";
+-- Group [Group]
+drop table "public"."group";
